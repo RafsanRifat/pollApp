8af28ee22b5d5edc68c3a33e41b5dc825ee5d4f8
RafsanRifat <rafsan.rifat255@gmail.com>
2022-06-15 15:55:19 +0600

    new views added

--- poll/views.py
+++ poll/views.py
@@ -3,9 +3,23 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+from poll.models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, This is the first view")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
 
 def detail(request, question_id):
     return HttpResponse("You are looking at question %s" % question_id)
+
+
+def result(request, question_id):
+    response = "You are looking at the results of question %s"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You are voting at %s" % question_id)
