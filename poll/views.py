486c4733f12ddf2a007f23bf4ea53bd50116f59b
RafsanRifat <rafsan.rifat255@gmail.com>
2022-06-18 13:52:56 +0600

    poll app complete

--- poll/views.py
+++ poll/views.py
@@ -28,14 +28,15 @@ def detail(request, question_id):
 
 
 def result(request, question_id):
-    response = "You are looking at the results of question %s"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'poll/result.html', {'question': question})
 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        print(request.POST)
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form
         return render(request, 'poll/detail.html', {
